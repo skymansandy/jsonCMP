9ff72a053497d581feaf9ea82bdcfe1b8650e0f8
Sandesh <iamsandythedev@gmail.com>
2026-03-29 22:30:09 +0530

    Add selection

--- json-cmp/src/commonMain/kotlin/dev/skymansandy/jsoncmp/ui/viewer/component/JsonViewerLineList.kt
+++ json-cmp/src/commonMain/kotlin/dev/skymansandy/jsoncmp/ui/viewer/component/JsonViewerLineList.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.selection.DisableSelection
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
@@ -128,16 +129,18 @@ internal fun JsonViewerLineList(
             }
         }
 
-        LazyColumn(
-            state = listState,
-            modifier = Modifier
-                .fillMaxSize()
-                .horizontalScroll(horizontalScrollState),
-        ) {
-            items(
-                items = visibleLines,
-                key = { it.lineNumber },
-            ) { line -> lineContent(line) }
+        SelectionContainer {
+            LazyColumn(
+                state = listState,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .horizontalScroll(horizontalScrollState),
+            ) {
+                items(
+                    items = visibleLines,
+                    key = { it.lineNumber },
+                ) { line -> lineContent(line) }
+            }
         }
     }
 }
